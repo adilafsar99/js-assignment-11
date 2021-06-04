e3c423479bcc733351129f9180f79f7f0e26d33f
Muhammad Adil <adilafsar99@gmail.com>
2021-06-04 20:13:40 +0500

    Update app.js

--- app.js
+++ app.js
@@ -77,7 +77,11 @@ else if (taskNum === 2)
 {
   function isLeapYear(year)
   {
-    if (year % 4 === 0)
+    if (year % 4 === 0 && year % 100 !== 0)
+    {
+      return true;
+    }
+    else if (year % 100 === 0 && year % 400 == 0)
     {
       return true;
     }
